072cf189c36efff23f190db8ffc9cb8d3f42cd7e
Maikel Waibel <maikel.waibel@live.de>
2018-11-11 12:52:30 +0100

    store test

---
+++ src/pages/store-test.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page padding>
+    <!-- content -->
+    <h1> Store Test </h1>
+
+  </q-page>
+</template>
+
+<script>
+export default {
+  // name: 'PageName',
+};
+</script>
+
+<style>
+</style>
